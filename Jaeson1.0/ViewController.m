a0d3ecfea04202271684ecbf9de20dd65101211d
Leson <leson@LesondeMacBook-Pro.local>
2018-03-27 11:24:54 +0800

    提交一个关于rumtime的代码

--- Jaeson1.0/ViewController.m
+++ Jaeson1.0/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+
+typedef NSMutableArray<NSMutableArray<NSString *> *> CJIndexPathArray;
 
 @interface ViewController ()
+
+///
+@property (nonatomic,retain) CJIndexPathArray *indexPathArray;
 
 @end
 
@@ -16,9 +22,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    
+    self.indexPathArray = [NSMutableArray array];
+    
+    for (int i = 0; i<3; i++) {
+        NSMutableArray *rowArray = [NSMutableArray array];
+        for (int j = 0; j< 9; j++) {
+            [rowArray addObject:[NSString stringWithFormat:@"%d %d",i,j]];
+        }
+        [self.indexPathArray addObject:rowArray];
+    }
+    
+    NSLog(@"%@",self.indexPathArray);
+    
+    NSString *testStr = [self testRuntime];
+    
+    NSLog(@"runtime's testStr is %@",testStr);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (NSString *)testRuntime
+{
+    NSString *testStr = objc_getAssociatedObject(self, _cmd);
+    
+    
+    if (!testStr) {
+        testStr = @"testStr";
+        objc_setAssociatedObject(self, _cmd, testStr, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    }
+    
+    return testStr;
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
